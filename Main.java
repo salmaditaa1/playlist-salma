ddcea67e781e83a978fdc65cfcc9a6b0e1ee5b38
salmaditaa1 <12450124874@students.uin-suska.ac.id>
2026-01-01 21:01:09 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+    public class Main {
+        public static void main(String[] args) {
+            Playlist playlist = new Playlist();
+
+            playlist.addSong("blue valentine <NMIXX>");
+            playlist.addSong("you! <LANY>");
+            playlist.addSong("one last time <ariana grande>");
+
+            playlist.displayPlaylist();
+        }
+    }
+
+
