596e11b989467a0db36cc72a0505788079b8898e
salmaditaa1 <12450124874@students.uin-suska.ac.id>
2026-01-01 21:02:40 +0700

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,11 @@
+public class Song {
+
+        String title;
+        Song next;
+
+        public Song(String title) {
+            this.title = title;
+            this.next = null;
+        }
+    }
+
